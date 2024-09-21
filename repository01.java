2877a17b2923dee1224b18effea617d5e650f651
aisrhd <malikgunawan398@gmail.com>
2024-09-21 15:31:16 +0800

    repository01.java

---
+++ repository01.java
@@ -0,0 +1,19 @@
+public class repository01 {
+
+    public static void main(String[] args) {
+        
+        String nama; 
+        int umur;
+
+
+        nama = " Ais";
+        umur = 18;
+
+        
+        System.out.println( "Nama :" + nama);
+        System.out.println( "Umur :" + umur + " tahun");
+
+   
+ }
+}
+
